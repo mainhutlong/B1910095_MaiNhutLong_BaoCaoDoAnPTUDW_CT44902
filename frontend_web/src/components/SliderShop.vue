ac12a122e051c9511e4e22d4f8d720e327ea7607
mainhutlong <124886771+mainhutlong@users.noreply.github.com>
2023-05-02 17:59:31 -0700

    Du an Phat Trien Ung Dung Web

---
+++ frontend_web/src/components/SliderShop.vue
@@ -0,0 +1,16 @@
+<template>
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://file.hstatic.net/200000097517/collection/banner1_9287ef0eb8744148a4e775f2c79ba245.jpg" class="d-block w-100 h-100" alt="...">
+    </div>
+  </div>
+</div>
+</template>
+<style scoped>
+    .carousel-item{
+        height: 600px;
+    }
+</style>
